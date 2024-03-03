dad0d903fdee3bd0aefa140ef844c6637a4e6c5f
palatipjant <palatipjant@outlook.com>
2024-03-03 19:24:05 +0700

    add new chall

---
+++ meditation-clock/meditation-clock/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  meditation-clock
+//
+//  Created by Palatip Jantawong on 3/3/2567 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ meditation-clock/meditation-clock/meditation_clockApp.swift
@@ -0,0 +1,17 @@
+//
+//  meditation_clockApp.swift
+//  meditation-clock
+//
+//  Created by Palatip Jantawong on 3/3/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct meditation_clockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
