0509ce77a02a52a81c8da6e40ce8dac4e690a553
palatipjant <palatipjant@outlook.com>
2024-03-07 15:31:05 +0700

    new projects

---
+++ Coffee Tracker/Coffee Tracker/Coffee_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Coffee_TrackerApp.swift
+//  Coffee Tracker
+//
+//  Created by Palatip Jantawong on 7/3/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct Coffee_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
