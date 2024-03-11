f7d1b75489a4348b79132866857fbce399bbaf97
palatipjant <palatipjant@outlook.com>
2024-03-11 16:04:24 +0700

    setting up firebase

--- Social-App/Social-App/Views/LoginView.swift
+++ Social-App/Social-App/Views/LoginView.swift
@@ -29,16 +29,85 @@ struct LoginView: View {
                     .scale(1.4)
                     .foregroundStyle(.white)
                 VStack(spacing: 16, content: {
-                    Text(isLoginMode ? "Login" : "Create Username")
+                    Text(isLoginMode ? "Login" : "Create Account")
                         .font(.largeTitle)
                         .bold()
                     
                     // Image Picker is LoginMode is false
+                    if !isLoginMode {
+                        Button(action: {
+                            shouldShowImagePicker.toggle()
+                        }, label: {
+                            VStack{
+                                if let image = image {
+                                    Image(uiImage: image)
+                                        .resizable()
+                                        .scaledToFill()
+                                        .frame(width: 128, height: 128)
+                                        .clipShape(RoundedRectangle(cornerRadius: 64))
+                                } else {
+                                    Image(systemName: "person.fill")
+                                        .font(.system(size: 64))
+                                        .padding()
+                                        .foregroundStyle(.black)
+                                }
+                            }.overlay {
+                                RoundedRectangle(cornerRadius: 64)
+                                    .stroke(.black, lineWidth: 3)
+                            }
+                        })
+                    }
                     
                     TextField("Email", text: $email)
+                        .keyboardType(.emailAddress)
+                        .textInputAutocapitalization(.none)
+                        .bold()
+                        .padding()
+                        .frame(width: 300, height: 50)
+                        .background(.black.opacity(0.05))
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                    
+                    SecureField("Password", text: $password)
+                        .textInputAutocapitalization(.none)
+                        .autocorrectionDisabled(true)
+                        .bold()
+                        .padding()
+                        .frame(width: 300, height: 50)
+                        .background(.black.opacity(0.05))
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                    
+                    Button(action: {
+                        //
+                    }, label: {
+                        Text(isLoginMode ? "Login" : "Create account")
+                            .foregroundStyle(.white)
+                            .padding(10)
+                            .frame(width: 300, height: 50)
+                            .background(.blue)
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                            .padding(.top, 20)
+                    })
+                    
+                    HStack{
+                        Text(isLoginMode ? "Don't have an account yet." : "Have an account?")
+                        
+                        Button(action: {
+                            isLoginMode.toggle()
+                        }, label: {
+                            Text(isLoginMode ? "Create account" : "Login")
+                        })
+                    }
+                    
+                    Text(loginStateMessage)
+                        .foregroundStyle(.red)
+                    
                     
                 }).padding()
             }
+            .fullScreenCover(isPresented: $shouldShowImagePicker, content: {
+                ImagePicker(image: $image)
+                    .ignoresSafeArea()
+            })
         }
     }
 }
