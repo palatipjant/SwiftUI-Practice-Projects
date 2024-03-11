f8e6f294ea29c09de3e468d958c6a822cf211ac8
palatipjant <palatipjant@outlook.com>
2024-03-11 12:41:18 +0700

    clean pro and init new pro

---
+++ Coin-Challenge/Coin-Challenge/Coin_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Coin_ChallengeApp.swift
+//  Coin-Challenge
+//
+//  Created by Palatip Jantawong on 11/3/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct Coin_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Coin-Challenge/Coin-Challenge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Coin-Challenge
+//
+//  Created by Palatip Jantawong on 11/3/2567 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
