3e84268952d664920e3351a7d5e69a5ad7037c38
palatipjant <palatipjant@outlook.com>
2024-03-28 20:39:51 +0700

    start postview

--- Social-App/Social-App/Views/PostView.swift
+++ Social-App/Social-App/Views/PostView.swift
@@ -7,6 +7,20 @@
 
 import SwiftUI
 
+struct PostComponeny: View{
+    
+    let post: Post
+    var postViewModel = PostViewModel()
+    @State private var profileImage: UIImage?
+    @State private var isLoadingImage = false
+    
+    var body: some View{
+        VStack{
+            
+        }
+    }
+}
+
 struct PostView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
